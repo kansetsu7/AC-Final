da23df11b247eb1cfce848dcb93abfc93f2d771e
Kansetsu Kuo <kansetsu4@gmail.com>
2018-04-10 15:53:23 +0800

    Done: set up association for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :posts
+  has_many :comments
+  has_many :commented_posts, through: :comment, source: :post
+  has_many :collects
+  has_many :collected_posts, through: :collects, source: :post
+  has_many :views
+  has_many :viewed_posts, through: :views, source: :post
+  has_many :friendships
+  has_many :friends, through: :friendships
+  has_many :confirmed_friendships, -> {where status: 'confirmed'}, class_name: "Friendship"
+  has_many :unconfirmed_friendships, -> {where status: 'unconfirmed'}, class_name: "Friendship"
+  has_many :confirmed_friends, through: :confirmed_friendships, source: :friend
+  has_many :unconfirmed_friends, through: :unconfirmed_friendships, source: :friend
+  has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id"
+  has_many :inverse_friends, through: :inverse_friendships, source: :user
+  has_many :confirmed_inverse_friendships, -> {where status: 'confirmed'}, class_name: "Friendship", foreign_key: "friend_id"
+  has_many :unconfirmed_inverse_friendships, -> {where status: 'unconfirmed'}, class_name: "Friendship", foreign_key: "friend_id"
+  has_many :confirmed_inverse_friends, through: :confirmed_inverse_friendships, source: :user
+  has_many :unconfirmed_inverse_friends, through: :unconfirmed_inverse_friendships, source: :user
 end
