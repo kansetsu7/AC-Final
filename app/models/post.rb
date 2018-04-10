16fb8bb56caec4131b722588ef5559abbdb1e2e4
Kansetsu Kuo <kansetsu4@gmail.com>
2018-04-10 16:55:29 +0800

    Done: set up association for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+  belongs_to :user
+  has_many :tags
+  has_many :tagged_categories, through: :tags, source: :category
+  has_many :replies
+  has_many :replied_users, through: :comments, source: :user
 end
