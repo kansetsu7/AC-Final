98df92df592832c868e9e66b2e86dad274ddfbcc
Kansetsu Kuo <kansetsu4@gmail.com>
2018-04-12 15:21:05 +0800

    Done: list all posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: :index
 
   def index
-    
+    @posts = Post.all.page(params[:page]).per(20)
   end
 
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post, counter_cache: :replies_count
+  # scope :latest, -> {order('created_at DESC').first}
+  scope :latest_time, -> {order('created_at DESC').first.created_at.strftime('%F')}
 end
