7e259c9ec8df610f7ceff6030ec19d6904b4d476
Kansetsu Kuo <kansetsu4@gmail.com>
2018-04-17 15:54:53 +0800

    Done: remove model tag

--- app/models/tag.rb
+++
@@ -1,5 +0,0 @@
-class Tag < ApplicationRecord
-  belongs_to :post
-  belongs_to :category
-  validates :category, uniqueness: { scope: :post_id }
-end
--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 20180412091011) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "post_id"
   end
 
   create_table "collects", force: :cascade do |t|
@@ -52,13 +53,6 @@ ActiveRecord::Schema.define(version: 20180412091011) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
-  end
-
-  create_table "tags", force: :cascade do |t|
-    t.integer "post_id"
-    t.integer "category_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
