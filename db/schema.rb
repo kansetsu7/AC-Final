f4e1d581af664279280dc2d221a8fbccc5acc210
Kansetsu Kuo <kansetsu4@gmail.com>
2018-04-17 15:58:58 +0800

    Done: rails g migration CreateJoinTableCategoriesPost categories post

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180417075522) do
+ActiveRecord::Schema.define(version: 20180417075754) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "categories_posts", id: false, force: :cascade do |t|
+    t.integer "category_id", null: false
+    t.integer "post_id", null: false
   end
 
   create_table "collects", force: :cascade do |t|
