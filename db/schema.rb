1863949a3231d98f656cb7724c34f16fa4d72f1e
Kansetsu Kuo <kansetsu4@gmail.com>
2018-04-17 15:56:26 +0800

    Done: rails g migration remove_post_id_from_categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180412091011) do
+ActiveRecord::Schema.define(version: 20180417075522) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "post_id"
   end
 
   create_table "collects", force: :cascade do |t|
