6fd0ae14a3ef0642572e3aaa118d8ba9ab8e515f
Kansetsu Kuo <kansetsu4@gmail.com>
2018-04-30 15:36:20 +0800

    Done: show user's collects

--- app/controllers/collects_controller.rb
+++ app/controllers/collects_controller.rb
@@ -1,2 +1,29 @@
 class CollectsController < ApplicationController
+  before_action :set_collect ,only: :destroy
+  before_action :set_user ,only: :index
+
+  def index
+    @collects = @user.collects
+  end
+
+  def destroy
+    if @collect.user == current_user
+      @collect.destroy
+      flash[:alert] = 'collected!'
+    else
+      flash[:alert] = "you can't uncollect other's collect"
+    end
+    
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def set_collect
+    @collect = Collect.find(params[:id])
+  end
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
 end
