31a419a4a7205a0c9689c80a729d52225f9f476e
Kansetsu Kuo <kansetsu4@gmail.com>
2018-05-01 10:21:39 +0800

    Done: set post title as 'No title' if no title

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   # mount_uploader :image, PostImageUploader
+  before_save :check_title
   belongs_to :user
   has_and_belongs_to_many :categories
   has_many :comments
@@ -8,5 +9,9 @@ class Post < ApplicationRecord
 
   def create_date
     self.created_at.strftime('%F')
+  end
+
+  def check_title
+    self.title = 'No title' if self.title == ''
   end
 end
