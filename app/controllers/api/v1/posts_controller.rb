8a68ecb13b65006518627ad10e8b6d635bc5d914
Kansetsu Kuo <kansetsu4@gmail.com>
2018-05-04 14:13:22 +0800

    Done: api update and destroy post

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::PostsController < ApiController
+  before_action :set_post, only: [:update, :destroy]
   def index
     @posts = Post.all
     render json: {
@@ -10,7 +11,7 @@ class Api::V1::PostsController < ApiController
     @post = Post.find_by(id: params[:id])
     if !@post
       render json: {
-        message: 'cannot find post!',
+        message: 'post not found!',
         status: 400
       }
     else
@@ -34,9 +35,45 @@ class Api::V1::PostsController < ApiController
     end
   end
 
+  def update
+    if @post.nil?
+      render json: {
+        message: 'post not found!',
+        status: 400
+      }
+    elsif @post.update(post_params)
+      render json: {
+        message: 'successfully updated!',
+        result: @post
+      }
+    else
+      render json: {
+        errors: @post.errors
+      }
+    end
+  end
+  
+  def destroy
+    if @post.nil?
+      render json: {
+        message: 'post not found!',
+        status: 400
+      }
+    else
+      @post.destroy
+      render json: {
+        message: 'successfully delete!'
+      }
+    end
+  end
+
   private
 
     def post_params
-      params.permit(:user_id, :title, :content, :status, :authority, :image, category_ids:[])
+      params.permit(:id, :user_id, :title, :content, :status, :authority, :image, category_ids:[])
+    end
+
+    def set_post
+      @post = Post.find_by(id: params[:id])
     end
 end
